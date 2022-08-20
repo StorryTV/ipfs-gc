1215c0a4b500668b7d7661a4f539e734139dea59
StorryTV <60582682+StorryTV@users.noreply.github.com>
2022-08-21 01:26:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -92,5 +92,5 @@ if [ ${command_failed:-0} -eq 1 ]; then
         echo "Failed: mv \"${CURRENT_DIR}/ipfs-gc\" /usr/bin/"
         command_failed=0
 else
-        echo 'IPFS GC has been sucesfully installed! You can also run it manually by entering: ipfs-gc -u ipfsbob as root or using sudo: sudo ipfs-gc -u ipfsbob'
+        echo 'IPFS GC has been succesfully installed! You can also run it manually by entering: ipfs-gc -u ipfsbob as root or using sudo: sudo ipfs-gc -u ipfsbob'
 fi
