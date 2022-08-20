1bf7667151333286b0d304ab9e361f79101937ed
StorryTV <60582682+StorryTV@users.noreply.github.com>
2022-08-21 01:09:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,20 +10,17 @@ else
 		command_failed=0
 	fi
 fi
-echo -n 'Do you want to run IPFS GC Automatically at a given schedule? (y/n) '
-echo -n ': '
+echo -n 'Do you want to run IPFS GC Automatically at a given schedule? (y/n): '
 read VAR
 if [[ $VAR = 'y' ]]; then
 	echo ''
         echo -n 'Do you want to run IPFS GC hourly(h), daily(d), weekly(w) or monthly(m)? (h/d/w/m): '
-	echo -n ': '
         read VAR
         if [[ $VAR = 'h' ]]; then
                 VAR_1='0 * * * *'
         elif [[ $VAR = 'd' ]]; then
 		echo ''
                 echo -n 'At what hour? (Choose from 00 to 23): '
-		echo -n ': '
                 read VAR
                 if [[ $VAR_ -lt 24 ]]; then
                         VAR_1="0 ${VAR} * * *"
@@ -31,12 +28,10 @@ if [[ $VAR = 'y' ]]; then
         elif [[ $VAR = 'w' ]]; then
 		echo ''
                 echo -n 'What day of the week: Monday(1), Tuesday(2), Wednesday(3), Thursday(4), Friday(5), Saturday(6), Sunday(7)? (Choose from 1 to 7): '
-		echo -n ': '
                 read VAR
                 if [[ $VAR -lt 8 ]]; then
 			echo ''
                         echo -n 'At what hour? (Choose from 00 to 23): '
-			echo -n ': '
                         read VAR_
                         if [[ $VAR_ -lt 24 ]]; then
                                 VAR_1="0 ${VAR_} * * ${VAR}"
@@ -45,12 +40,10 @@ if [[ $VAR = 'y' ]]; then
         elif [[ $VAR = 'm' ]]; then
 		echo ''
                 echo -n 'What day of the month? (Choose from 1 to 28): '
-		echo -n ': '
                 read VAR
                 if [[ $VAR -lt 32 ]]; then
 			echo ''
                         echo -n 'At what hour? (Choose from 00 to 23): '
-			echo -n ': '
                         read VAR_
                         if [[ $VAR_ -lt 24 ]]; then
                                 VAR_1="0 ${VAR_} ${VAR} * *"
@@ -59,24 +52,20 @@ if [[ $VAR = 'y' ]]; then
         fi
 	echo ''
         echo -n "Does your ipfs daemon run under the following user: $(whoami) (y/n): "
-	echo -n ': '
 	read VAR
 	if [[ $VAR = 'y' ]]; then
 		IPFS_GC_PARAMS_NAME="$(whoami)"
 	elif [[ $VAR = 'n' ]]; then
 		echo -n "Please enter the username the ipfs daemon runs under: "
-		echo -n ': '
-		read $VAR
-		IPFS_GC_PARAMS_NAME=" -u ${VAR}"
+		read $VAR_
+		IPFS_GC_PARAMS_NAME=" -u $VAR_"
 	fi
 	echo ''
         echo -n "Enter how much free space has to be left for IPFS GC to run (in Kilobytes/kb!): "
-	echo -n ': '
 	read VAR
 	IPFS_GC_PARAMS_STORAGE=" -s ${VAR}"
 	echo ''
         echo -n "Please enter the full path to the IPFS blocks for correct disk size calculations (if you have a standard IPFS setup you can just enter 'n') : "
-	echo -n ': '
 	read VAR
 	if [[ $VAR = 'n' ]]; then
 		IPFS_GC_PARAMS_PATHTOBLOCKS=''
